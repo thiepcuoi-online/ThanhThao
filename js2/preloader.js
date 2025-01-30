db21b435aac6a91a49dba303240e05113325df4b
Lqlam2102 <lqlam28@gmail.com>
2025-01-31 00:44:30 +0700

    first commit

---
+++ js2/preloader.js
@@ -0,0 +1,7 @@
+ // preloader
+/*
+ const preloader  = document.getElementById('preloader')
+ document.addEventListener("DOMContentLoaded",setTimeout(function pre() {
+ preloader.classList.add('uk-hidden')
+ 
+ },500) );*/
